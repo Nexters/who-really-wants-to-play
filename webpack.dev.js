7ab60b4b491b1f6807dbd0c877b3b3545922cd7b
1ilsang <1ilsang@naver.com>
2023-01-14 15:11:10 +0900

    chore(webpack): Fix eslint rule

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
-const { merge } = require('webpack-merge');
-
 const common = require('./webpack.common.js');
+
+const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
   mode: 'development',
