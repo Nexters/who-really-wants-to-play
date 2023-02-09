bce76b1f98c9e37b449c1f3945461ea28a0bb5e9
동동이 <drdragon777@naver.com>
2023-02-09 15:58:25 +0900

    feat(gallery): Add title animation logic

--- src/features/gallery/index.tsx
+++ src/features/gallery/index.tsx
@@ -4,13 +4,22 @@ import SubTitle from '../shared/components/SubTitle';
 
 import MasonryLayout from './components/MasonryLayout';
 import { GRID_GAP } from './constants';
+import { useTitleAnimation } from './hooks/useTitleAnimation';
+import { getTranslateX } from './helpers';
 
 import { GalleryMockImgList } from '~/features/gallery/mocks/gallery';
 
 const Gallery: FunctionComponent = () => {
+  const { titleRef, overflowedWidth } = useTitleAnimation(200);
+
   return (
     <section className="gallery">
-      <SubTitle title="Our Archive" />
+      <SubTitle
+        ref={titleRef}
+        title="Our Archive"
+        className="title-animation"
+        style={{ transform: getTranslateX(overflowedWidth) }}
+      />
       <MasonryLayout imgList={GalleryMockImgList} gap={GRID_GAP} />
     </section>
   );
