34ae6ba4b98c372ad80ad02bfc3b73bd834bd764
동동이 <drdragon777@naver.com>
2023-02-09 21:36:25 +0900

    feat(gallery): Add Image aspect-ratio

--- src/features/shared/components/Image/index.tsx
+++ src/features/shared/components/Image/index.tsx
@@ -5,7 +5,13 @@ import { ImgProps } from '~/types';
 
 const Image = (props: ImgProps, ref: LegacyRef<HTMLImageElement> | null) => {
   const { src, alt, width, height, className } = props;
-  return <img {...{ className, src, alt, width, height, ref }} />;
+  const aspectRatio = width && height ? width / height : 0;
+  return (
+    <img
+      {...{ className, src, alt, width, height, ref }}
+      style={{ aspectRatio }}
+    />
+  );
 };
 
 export default forwardRef(Image);
