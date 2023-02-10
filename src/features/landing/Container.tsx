24a73b7f4f9ad646bee75dffb06ca1eb75512a6f
조예진 <orobos654@gmail.com>
2023-02-11 01:50:39 +0900

    feat: Add wheel animation for page transition

--- src/features/landing/Container.tsx
+++ src/features/landing/Container.tsx
@@ -3,15 +3,24 @@ import { FunctionComponent, useState } from 'react';
 import BackgroundGraphicSvg from '~/features/landing/components/svg/BackgroundGraphicSvg';
 import ScrollToEnter from '~/features/landing/components/ScrollToEnter';
 import ExpandableCircle from '~/features/landing/components/ExpandableCircle';
+import { LANDING_LAST_ANIMATION_NAME } from '~/features/landing/constants';
 
 const LandingContainer: FunctionComponent = () => {
-  const [active, setActive] = useState(false);
+  const [canMoveNext, setCanMoveNext] = useState(false);
+  const [isTriggeredTransition, setTriggeredTransition] = useState(false);
+
   return (
-    <div className="landing-container" onClick={() => setActive(true)}>
+    <div
+      className="landing-container"
+      onAnimationEnd={({ animationName }) => {
+        if (animationName === LANDING_LAST_ANIMATION_NAME) setCanMoveNext(true);
+      }}
+      onWheel={() => canMoveNext && setTriggeredTransition(true)}
+    >
       <BackgroundGraphicSvg />
       <h1 className="landing-title">Jjin-Nolsa</h1>
       <ScrollToEnter />
-      <ExpandableCircle expand={active} />
+      <ExpandableCircle expand={isTriggeredTransition} />
     </div>
   );
 };
