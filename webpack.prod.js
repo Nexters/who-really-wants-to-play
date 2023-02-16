bf16401125f43cad43bc850a2b25330db111165b
1ilsang <1ilsang@naver.com>
2023-02-16 20:07:41 +0900

    chore(webpack): Change webpack import order

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
-const common = require('./webpack.common.js');
-
 const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
 
 const productionOption = {
   mode: 'production',
