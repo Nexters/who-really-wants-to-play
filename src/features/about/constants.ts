035ca8fe873bd4f6527f076f9748b60c9e04b171
anyl92 <yulim9592@gmail.com>
2023-02-25 08:47:09 +0900

    dump

---
+++ src/features/about/constants.ts
@@ -0,0 +1,52 @@
+export const TITLE = 'About us';
+
+const PROFILES_LIST = [
+  {
+    name: 'Kim Yeonghwan',
+    job: 'Designer',
+  },
+  {
+    name: 'Lee Hyebin',
+    job: 'Designer',
+  },
+  {
+    name: 'Lee Sangchul',
+    job: 'Developer',
+  },
+  {
+    name: 'An Yulim',
+    job: 'Developer',
+  },
+  {
+    name: 'Kim Dongyong',
+    job: 'Developer',
+  },
+  {
+    name: 'Cho Yejin',
+    job: 'Developer',
+  },
+];
+
+const BLANK_LIST = [
+  {
+    name: '',
+    job: '',
+  },
+  {
+    name: '',
+    job: '',
+  },
+  {
+    name: '',
+    job: '',
+  },
+]
+
+export const PROFILES_REPEAT = [
+  ...BLANK_LIST,
+  ...PROFILES_LIST,
+  ...PROFILES_LIST,
+  ...PROFILES_LIST,
+  ...BLANK_LIST,
+  ...BLANK_LIST,
+];
