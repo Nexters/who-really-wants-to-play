3f99709dec29ba262eaf9143660b5208c6d0c26a
anyl92 <yulim9592@gmail.com>
2023-02-27 05:18:01 +0900

    feat(constants): set page name value

--- src/features/constants.ts
+++ src/features/constants.ts
@@ -1,6 +1,5 @@
 export const ACTIVE_INDEX_MAP = {
   0: 'LANDING',
-  // NOTE: activeIndex가 더 늘어날 예정.
   1: 'DAILY_BOOK',
   2: 'DAILY_BOOK',
   3: 'DAILY_BOOK',
@@ -12,12 +11,17 @@ export const ACTIVE_INDEX_MAP = {
   9: 'DAILY_BOOK',
   10: 'DAILY_BOOK',
   11: 'DAILY_BOOK',
-  12: 'GALLERY',
-  13: 'ABOUT',
+  12: ['GALLERY', 'ABOUT'],
 };
+
+const MAP_LENGTH = Object.keys(ACTIVE_INDEX_MAP).length;
+const DAILY_BOOK_ARRAY = [];
+for (let i = 1; i < MAP_LENGTH - 1; i++) {
+  DAILY_BOOK_ARRAY.push(i);
+}
+
 export const PAGE_NAME = {
   LANDING: 0,
-  DAILY_BOOK: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-  GALLERY: 12,
-  ABOUT: 13,
+  DAILY_BOOK: DAILY_BOOK_ARRAY,
+  BOTTOM: MAP_LENGTH - 1,
 };
