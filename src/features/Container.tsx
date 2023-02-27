2207e44268c26c6e356136af03a57536d7352d03
동동이 <drdragon777@naver.com>
2023-02-27 18:35:55 +0900

    feat(gallery-interaction): Add container style, props, scrollSnapContainer according to needScrollSnap condition

--- src/features/Container.tsx
+++ src/features/Container.tsx
@@ -1,15 +1,24 @@
+import classnames from 'classnames';
 import { FunctionComponent } from 'react';
 
 import DailyBookContainer from './dailyBook/Container';
 import GalleryContainer from './gallery/Container';
-import useAppContainer from './hooks/useAppContainer';
 import LandingContainer from './landing/Container';
+import { PAGE_NAME } from './shared/constants';
+import useAppContainer from './shared/hooks/useAppContainer';
 
 const AppContainer: FunctionComponent = () => {
   const appData = useAppContainer();
+  const { containerRef, activeIndex } = appData;
+  const { GALLERY, ABOUT } = PAGE_NAME;
+  const needScrollSnap = activeIndex !== GALLERY && activeIndex !== ABOUT;
+  const scrollSnapContainer = needScrollSnap ? 'scroll-snap-container' : '';
 
   return (
-    <div className="scroll-snap-container">
+    <div
+      className={classnames('container', scrollSnapContainer)}
+      ref={containerRef}
+    >
       <LandingContainer {...appData} />
       <DailyBookContainer {...appData} />
       <GalleryContainer {...appData} />
