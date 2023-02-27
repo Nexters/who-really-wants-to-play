6c53046385b109b571c5c17f83d82014b19ecd63
동동이 <drdragon777@naver.com>
2023-02-27 18:39:11 +0900

    feat(gallery-interaction): Add animationRatio logic according to percent of distance scrolled

--- src/features/gallery/Container.tsx
+++ src/features/gallery/Container.tsx
@@ -1,13 +1,13 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useRef } from 'react';
 
 import SubTitle from '../shared/components/SubTitle';
 import { AppData } from '../types';
-import { PAGE_NAME } from '../constants';
+import { PAGE_NAME } from '../shared/constants';
 
 import MasonryLayout from './components/MasonryLayout';
 import { GRID_GAP } from './constants';
-import { useTitleAnimation } from './hooks/useTitleAnimation';
-import { getTranslateX } from './helpers';
+import { getMinusTranslateX } from './helpers';
+import { useGalleryScrollRatio } from './hooks/useGalleryScrollRatio';
 
 import { GalleryMockImgList } from '~/features/gallery/mocks/gallery';
 
@@ -15,8 +15,16 @@ type DailyBookContainerProps = AppData;
 
 const GalleryContainer: FunctionComponent<DailyBookContainerProps> = ({
   refList,
+  containerRef,
 }) => {
-  const { titleRef, overflowedWidth } = useTitleAnimation(200);
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const titleWidth = titleRef.current?.scrollWidth || 0;
+  const animationRatio = useGalleryScrollRatio(
+    // TODO: 불필요한 상태 변경 최적화, useState대신 useRef로 스크롤 이벤트 사용 필요성
+    containerRef,
+    refList.current[PAGE_NAME.GALLERY],
+  );
+  const titleMustMoveDist = titleWidth * animationRatio;
 
   return (
     <section
@@ -30,8 +38,10 @@ const GalleryContainer: FunctionComponent<DailyBookContainerProps> = ({
       <SubTitle
         ref={titleRef}
         title="Our Archive"
-        className="title-animation"
-        style={{ transform: getTranslateX(overflowedWidth) }}
+        className="gallery-title title-animation"
+        style={{
+          transform: getMinusTranslateX(titleMustMoveDist, 'px'),
+        }}
       />
       <MasonryLayout imgList={GalleryMockImgList} gap={GRID_GAP} />
     </section>
---
+++ src/features/gallery/hooks/useGalleryScrollRatio.ts
@@ -0,0 +1,39 @@
+import { RefObject, useEffect, useState } from 'react';
+
+import { throttle } from '~/features/shared/utils/throttle';
+
+export const useGalleryScrollRatio = (
+  containerRef: RefObject<HTMLDivElement>,
+  galleryRef: HTMLElement,
+) => {
+  const [scrollY, setScrollY] = useState(0);
+  const [animationRatio, setAnimationRatio] = useState(0);
+
+  useEffect(() => {
+    const scrollSnapContainer = containerRef.current;
+    if (!scrollSnapContainer) return;
+
+    const handleScroll = (e: Event) => {
+      const containerDiv = e.target as HTMLDivElement;
+      setScrollY(containerDiv.scrollTop);
+    };
+    scrollSnapContainer.addEventListener('scroll', throttle(handleScroll, 100));
+    return () => {
+      scrollSnapContainer.removeEventListener('scroll', handleScroll);
+    };
+  }, [containerRef]);
+
+  useEffect(() => {
+    if (!galleryRef) return;
+    const galleryAbsoluteTop = galleryRef?.offsetTop;
+    const galleryMovedDist = scrollY - galleryAbsoluteTop;
+    const galleryHeight = galleryRef?.clientHeight;
+    const scrollbarHeight = window.innerHeight;
+    const heightWithScrollbarRemoved = galleryHeight - scrollbarHeight;
+    const galleryMovedScrollRatio =
+      galleryMovedDist / heightWithScrollbarRemoved;
+    setAnimationRatio(galleryMovedScrollRatio);
+  }, [galleryRef, scrollY]);
+
+  return animationRatio;
+};
